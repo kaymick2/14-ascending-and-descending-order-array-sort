6ab13090131d1e9cbff145b5d11e4a91cabbcc04
Kyle McDowell <>
2021-04-12 05:06:16 +0000

    working but not original

--- Main.java
+++ Main.java
@@ -1,18 +1,18 @@
 import java.util.Scanner;
+import java.util.Random;
 public class Main
 {
     public static void main(String[] args) 
     {
         int n, temp;
+        Random rNGesus=new Random();
         Scanner s = new Scanner(System.in);
         System.out.print("Enter no. of elements you want in array:");
         n = s.nextInt();
         int a[] = new int[n];
-        System.out.println("Enter all the elements:");
-        for (int i = 0; i < n; i++) 
-        {
-            a[i] = s.nextInt();
-        }
+    for (int x = 0; x <= (a.length) - 1; x++) {
+      a[x] = rNGesus.nextInt(n);
+    }
         for (int i = 0; i < n; i++) 
         {
             for (int j = i + 1; j < n; j++) 
