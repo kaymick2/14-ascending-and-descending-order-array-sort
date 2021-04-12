8b9fba0bfde5b09f513d778d54bdc6dc15540472
Kyle McDowell <>
2021-04-12 04:56:09 +0000

    placeholder

--- Main.java
+++ Main.java
@@ -9,24 +9,19 @@ public class Main {
     Scanner in = new Scanner(System.in);
     Random rNGesus = new Random();
     System.out.println("enter array length here:");
-    int arrayLength=in.nextInt();
+    int arrayLength = in.nextInt();
     int a[] = new int[10];
     for (int x = 0; x <= (a.length) - 1; x++) {
       a[x] = rNGesus.nextInt(arrayLength);
     }
     System.out.println("The unsorted array is: " + Arrays.toString(a));
-    for (int j = 0; j < a.length - 1; j++) {
-      int iMin = j;
-      for (int i = j + 1; i < a.length; i++) {
-        if (a[i] < a[iMin]) {
-          iMin = i;
-          System.out.println("Progress: " + Arrays.toString(a));
+    for (int i = 0; i < n; i++) {
+      for (int j = i + 1; j < n; j++) {
+        if (a[i] < a[j]) {
+          temp = a[i];
+          a[i] = a[j];
+          a[j] = temp;
         }
-      } // you need another close } here, must close this loop before the if
-      if (iMin != j) {
-        int tempVar = a[iMin];
-        a[iMin] = a[j];// this i needs to j
-        a[j] = tempVar;// this line is backwards
       }
     }
     System.out.println("the sorted array is:" + Arrays.toString(a));
