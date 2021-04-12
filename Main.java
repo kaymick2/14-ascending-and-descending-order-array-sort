eaf91e2c3f0362c02eba745e324916c1126e04d3
Kyle McDowell <>
2021-04-12 05:02:22 +0000

    huh

--- Main.java
+++ Main.java
@@ -1,30 +1,35 @@
-import java.util.Arrays;
-import java.util.Random;
 import java.util.Scanner;
-
-
-/**
- * This program demonstrates the linear search algorithm.
- */
-public class Main {
-  public static void main(String[] args) {
-    Scanner in = new Scanner(System.in);
-    Random rNGesus = new Random();
-    System.out.println("enter array length here:");
-    int n = in.nextInt();
-    int a[] = new int[n];
-    for (int x = 0; x <= (a.length) - 1; x++) {
-      a[x] = rNGesus.nextInt(70);
-    }
-    System.out.println("The unsorted array is: " + Arrays.toString(a));
-    Arrays.sort(a);
-    System.out.println("the sorted array is:");
-    System.out.println("ascending order: "+ Arrays.toString(a));
-    System.out.print("descending order:");
+public class Main
+{
+    public static void main(String[] args) 
+    {
+        int n, temp;
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter no. of elements you want in array:");
+        n = s.nextInt();
+        int a[] = new int[n];
+        System.out.println("Enter all the elements:");
+        for (int i = 0; i < n; i++) 
+        {
+            a[i] = s.nextInt();
+        }
+        for (int i = 0; i < n; i++) 
+        {
+            for (int j = i + 1; j < n; j++) 
+            {
+                if (a[i] < a[j]) 
+                {
+                    temp = a[i];
+                    a[i] = a[j];
+                    a[j] = temp;
+                }
+            }
+        }
+        System.out.print("Descending Order:");
         for (int i = 0; i < n - 1; i++) 
         {
             System.out.print(a[i] + ",");
         }
         System.out.print(a[n - 1]);
-  }
+    }
 }
