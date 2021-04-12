1010abfd0d63dd80118280f2a71aa2f7fc41dfa8
Kyle McDowell <>
2021-04-12 05:00:00 +0000

    broken

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
+
 
 /**
  * This program demonstrates the linear search algorithm.
@@ -9,21 +11,20 @@ public class Main {
     Scanner in = new Scanner(System.in);
     Random rNGesus = new Random();
     System.out.println("enter array length here:");
-    int arrayLength = in.nextInt();
-    int a[] = new int[10];
+    int n = in.nextInt();
+    int a[] = new int[n];
     for (int x = 0; x <= (a.length) - 1; x++) {
-      a[x] = rNGesus.nextInt(arrayLength);
+      a[x] = rNGesus.nextInt(70);
     }
     System.out.println("The unsorted array is: " + Arrays.toString(a));
-    for (int i = 0; i < n; i++) {
-      for (int j = i + 1; j < n; j++) {
-        if (a[i] < a[j]) {
-          temp = a[i];
-          a[i] = a[j];
-          a[j] = temp;
+    Arrays.sort(a);
+    System.out.println("the sorted array is:");
+    System.out.println("ascending order: "+ Arrays.toString(a));
+    System.out.print("descending order:");
+        for (int i = 0; i < n - 1; i++) 
+        {
+            System.out.print(a[i] + ",");
         }
-      }
-    }
-    System.out.println("the sorted array is:" + Arrays.toString(a));
+        System.out.print(a[n - 1]);
   }
 }
