4d69f1a6052456b5a49995346f6b96a917c70bea
Kyle McDowell <>
2021-04-12 05:56:20 +0000

    done

--- Main.java
+++ Main.java
@@ -1,35 +1,23 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.Random;
-public class Main
-{
-    public static void main(String[] args) 
-    {
-        int n, temp;
-        Random rNGesus=new Random();
-        Scanner s = new Scanner(System.in);
-        System.out.print("Enter no. of elements you want in array:");
-        n = s.nextInt();
-        int a[] = new int[n];
+
+public class Main {
+  public static void main(String[] args) {
+    int n, temp;
+    Random rNGesus = new Random();
+    Scanner in = new Scanner(System.in);
+    System.out.print("Enter no. of elements you want in array:");
+    n = in.nextInt();
+    int a[] = new int[n];
     for (int x = 0; x <= (a.length) - 1; x++) {
       a[x] = rNGesus.nextInt(n);
     }
-        for (int i = 0; i < n; i++) 
-        {
-            for (int j = i + 1; j < n; j++) 
-            {
-                if (a[i] < a[j]) 
-                {
-                    temp = a[i];
-                    a[i] = a[j];
-                    a[j] = temp;
-                }
-            }
-        }
-        System.out.print("Descending Order:");
-        for (int i = 0; i < n - 1; i++) 
-        {
-            System.out.print(a[i] + ",");
-        }
-        System.out.print(a[n - 1]);
+    Arrays.sort(a);
+    System.out.println("ascendo:" + Arrays.toString(a));
+    System.out.println("descendo:");
+    for (int i = a.length - 1; i >= 0; i--) {
+      System.out.print(a[i] + " ");
     }
+  }
 }
